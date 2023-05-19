9bc2556ff40ca753e8a138001f0e6d95d59d197c
최지연 <choi02647@naver.com>
2023-05-19 16:18:18 +0900

    Initial Commit

---
+++ RxSwiftPractice/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RxSwiftPractice
+//
+//  Created by 최지연/클라이언트 on 2023/05/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RxSwiftPractice/RxSwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwiftPracticeApp.swift
+//  RxSwiftPractice
+//
+//  Created by 최지연/클라이언트 on 2023/05/19.
+//
+
+import SwiftUI
+
+@main
+struct RxSwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
